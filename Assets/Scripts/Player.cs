6a37cb0cb4c672748b67393cab00fe67afead4a9
c2p-cmd <sharandt19@live.com>
2022-05-10 01:25:32 +0530

    Changes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,10 @@ public class Player : MonoBehaviour {
     public float maxY;
     public float minY;
 
-    private Vector2 _targetPos;
+    private Vector3 _targetPos;
 
     public int health;
+    private string _healthString;
 
     public GameObject moveEffect;
     public Animator camAnim;
@@ -20,27 +21,86 @@ public class Player : MonoBehaviour {
     public GameObject restartDisplay;
     private static readonly int Shake = Animator.StringToHash("shake");
 
+    public RectTransform dPad;
+    private Camera _camera;
+    private bool _isCameraNotNull;
+
+    private void Start()
+    {
+        _isCameraNotNull = _camera != null;
+        _camera = Camera.main;
+    }
+
+    private void Awake() {
+        _healthString = "♡ ♡ ♡";
+    }
+
     private void Update() {
-        if (health <= 0) {
+        if (health <= 0) { //health <= 0
             spawner.SetActive(false);
             restartDisplay.SetActive(true);
             Destroy(gameObject);
         }
 
-        healthDisplay.text = health.ToString();
+        _healthString = health switch {
+            1 => "♡    ",
+            2 => "♡ ♡  ",
+            3 => "♡ ♡ ♡",
+            _ => ""
+        };
+        healthDisplay.text = _healthString;
 
-        transform.position = Vector2.MoveTowards(transform.position, _targetPos, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _targetPos, speed * Time.deltaTime);
+        
+        var up = Input.GetKeyDown(KeyCode.UpArrow) || clickUp();
+        var down = Input.GetKeyDown(KeyCode.DownArrow) || clickDown();
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY) {
-            camAnim.SetTrigger(Shake);
-            var position = transform.position;
-            Instantiate(moveEffect, position, Quaternion.identity);
-            _targetPos = new Vector2(position.x, position.y + increment);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY) {
-            camAnim.SetTrigger(Shake);
-            var position = transform.position;
-            Instantiate(moveEffect, position, Quaternion.identity);
-            _targetPos = new Vector2(position.x, position.y - increment);
+        if (up && transform.position.y < maxY) {
+            goUp();
+        } else if (down && transform.position.y > minY) {
+            goDown();
         }
+        
+        transform.position = Vector3.ClampMagnitude(transform.position, new Vector3(0f, 5f).magnitude);
+    }
+
+    private bool clickUp() {
+        if (Input.touchCount == 0)
+            return false;
+        var touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Ended)
+            return false;
+        
+        var touchWorld = _camera.ScreenToWorldPoint(touch.position).y;
+        var dPadWorld = _camera.ScreenToWorldPoint(dPad.position).y;
+        Debug.Log(touchWorld > dPadWorld);
+        return touchWorld > dPadWorld;
+    }
+    
+    private bool clickDown() {
+        if (Input.touchCount == 0)
+            return false;
+        var touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Ended)
+            return false;
+        
+        var touchWorld = _camera.ScreenToWorldPoint(touch.position).y;
+        var dPadWorld = _camera.ScreenToWorldPoint(dPad.position).y;
+        Debug.Log(touchWorld < dPadWorld);
+        return touchWorld < dPadWorld;
+    }
+
+    private void goUp() {
+        camAnim.SetTrigger(Shake);
+        var position = transform.position;
+        Instantiate(moveEffect, position, Quaternion.identity);
+        _targetPos = new Vector3(position.x, position.y + increment);
+    }
+
+    private void goDown() {
+        camAnim.SetTrigger(Shake);
+        var position = transform.position;
+        Instantiate(moveEffect, position, Quaternion.identity);
+        _targetPos = new Vector3(position.x, position.y - increment);
     }
 }
